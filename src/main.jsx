9932b3ecdda0f82ac5dc519a305d6763e8beee74
Eric Besson <150351199+Eric198809@users.noreply.github.com>
2024-07-22 13:45:13 +0200

    API + form

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/likePage",
+        path: "/coups-de-coeur",
         element: <LikePage />,
       },
     ],
--- src/pages/components/Form.jsx
+++ src/pages/components/Form.jsx
@@ -3,35 +3,59 @@ import { useState } from "react";
 import { useEffect } from "react";
 import Card from "./Card";
 
-
 const Form = () => {
-const [movies, setMovies]= useState([])
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("retour vers le ");
+  const [sortGoodBad, setSortGoodBad] = useState(null);
 
   useEffect(() => {
-    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=code&language=fr-FR"`).then
-      ((res) => setMovies(res.data.results));
-  }, [])
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=${search}&language=fr-FR"`
+      )
+      .then((res) => setMovies(res.data.results));
+  }, [search]);
+
+  const searchMovie = (e) => {
+    setSearch(e.target.value);
+  };
+
+  
+
   return (
     <>
       <div className="form-component">
         <div className="form-container">
           <form>
-            <input type="text" placeholder="Rechercher un film" id="search-input" />
+            <input
+              type="text"
+              placeholder="Rechercher un film"
+              id="search-input"
+              onChange={searchMovie}
+            />
             <input type="submit" value="Rechercher" />
           </form>
           <div className="btn-sort-container">
-            <div className="btn-sort" id="goodToBad">
+            <div className="btn-sort" id="goodToBad" onClick={()=> setSortGoodBad("goodToBad")}>
               Top <span> - </span>
             </div>
-            <div className="btn-sort" id="badToGood">
+            <div className="btn-sort" id="badToGood" onClick={()=> setSortGoodBad("badToGood")}>
               Flop <span> - </span>
             </div>
           </div>
         </div>
         <div className="result">
-          {movies.slice(0,12).map((movie)=> (
-           <Card movie={movie} key={movie.id}/>
-          ))}
+          {movies
+            .slice(0, 12)
+            .sort((a, b) => {
+              if (sortGoodBad === "goodToBad") {
+                return b.vote_average - a.vote_average;
+              } else if (sortGoodBad === "badToGood")
+                return a.vote_average - b.vote_average;
+            })
+            .map((movie) => (
+              <Card movie={movie} key={movie.id} />
+            ))}
         </div>
       </div>
     </>
